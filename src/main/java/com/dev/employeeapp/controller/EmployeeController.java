817023ebcfd59141166b93110bf2390c7b4657cf
DinelyWellehewage <dinelywh78@gmail.com>
2025-01-23 23:52:54 +0100

    add logic to add employee

--- src/main/java/com/dev/employeeapp/controller/EmployeeController.java
+++ src/main/java/com/dev/employeeapp/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import com.dev.employeeapp.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -31,11 +32,19 @@ public class EmployeeController {
         return "employees/list-employees";
     }
 
-    @PostMapping("showFormForAdd")
+    @GetMapping("/showFormForAdd")
     public String showFormForAdd(Model model){
         Employee employee = new Employee();
         model.addAttribute("employee",employee);
 
         return "employees/employee-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee){
+
+        employeeService.save(employee);
+
+        return "redirect:/employees/list";
     }
 }
