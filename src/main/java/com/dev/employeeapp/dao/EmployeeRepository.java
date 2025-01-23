a5d07162e8e22213734b05a2bcedfd3d71718082
DinelyWellehewage <dinelywh78@gmail.com>
2025-01-23 23:55:23 +0100

    add sort by last name

--- src/main/java/com/dev/employeeapp/dao/EmployeeRepository.java
+++ src/main/java/com/dev/employeeapp/dao/EmployeeRepository.java
@@ -3,5 +3,13 @@ package com.dev.employeeapp.dao;
 import com.dev.employeeapp.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
+
+
+    //add a method to sort by last name
+    List<Employee> findAllByOrderByLastNameAsc();
+
+
 }
