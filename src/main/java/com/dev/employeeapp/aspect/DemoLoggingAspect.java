0487b3213d365d9656f776d8f15ace1938726703
DinelyWellehewage <dinelywh78@gmail.com>
2025-01-24 01:02:46 +0100

    add aop to after method

--- src/main/java/com/dev/employeeapp/aspect/DemoLoggingAspect.java
+++ src/main/java/com/dev/employeeapp/aspect/DemoLoggingAspect.java
@@ -2,9 +2,7 @@ package com.dev.employeeapp.aspect;
 
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 import java.util.logging.Logger;
@@ -31,15 +29,16 @@ public class DemoLoggingAspect {
     }
 
     @Pointcut("forControllerPackage() || forServicePackage() || forDaoPackage()")
-    private void forAppFlow(){}
+    private void forAppFlow() {
+    }
 
     //add @Before advice
     @Before("forAppFlow()")
-    public void before(JoinPoint joinPoint){
+    public void before(JoinPoint joinPoint) {
         String method = joinPoint.getSignature().toShortString();
 
         //display method we are calling
-        logger.info("========>> in @Before: calling method: "+method);
+        logger.info("========>> in @Before: calling method: " + method);
 
         //display the arguments to the method
 
@@ -48,12 +47,27 @@ public class DemoLoggingAspect {
         Object[] args = joinPoint.getArgs();
 
         //loop thru and display args
-        for (Object arg:args){
-            logger.info("=======>> arguments: "+arg);
+        for (Object arg : args) {
+            logger.info("=======>> arguments: " + arg);
         }
-
     }
 
+    //add #AFterReturning
+    @AfterReturning(
+            pointcut = "forAppFlow()",
+            returning = "theResult"
+    )
+    public void afterReturning(JoinPoint joinPoint, Object theResult) {
+
+        //display method we are returning from
+        String method = joinPoint.getSignature().toShortString();
+        logger.info("========>> in @AfterReturning: from method: " + method);
+
+        //display the data returned
+        logger.info("=======>>> result: " + theResult);
+
+
+    }
 
 
 }
